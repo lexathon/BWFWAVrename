9f7c4fc918448ba4eb39972785ab2ffb605289f7
lexathon <60646199+lexathon@users.noreply.github.com>
2022-09-04 12:13:40 +0100

    Upload v0.2b

--- bwfwavrename.py
+++ bwfwavrename.py
@@ -7,9 +7,6 @@
 # If you are working with polywav files, you'll need to separate them first
 # You can use BWF Manager from Fostex to do this:
 # https://www.fostexinternational.com/docs/tech_support/bwf_manager.shtml
-# ***CURRENTLY polywavs converted this way cannot be renamed properly****
-# ***because the polywav metadata is copied to all files - messy developers!****
-# ***I'm working on it!***
 
 # Instructions:
 # Install python if it is not on your system already
@@ -22,6 +19,7 @@
 import re
 import os
 import sys
+import xml.etree.cElementTree as ET
 
 path = os.path.abspath(sys.argv[1])
 
@@ -35,27 +33,34 @@ def fileHeader(file):
 	openfile = open(file, 'rb')
 	stream = openfile.read(5000)
 	openfile.close()
-	return stream.decode('utf-8', 'ignore')
-
-def xmlSplit(tag, data):
-	startTag = '<' + tag + '>'
-	endTag = '</' + tag + '>'
-	return((data.split(startTag))[1].split(endTag)[0])
+	headerData = stream.decode('utf-8', 'ignore')
+	return '<?xml version' + headerData.split('<?xml version',1)[1]
 
 def newName(file):
-	trackList = xmlSplit('TRACK_LIST', fileHeader(file))
-	if xmlSplit('TRACK_COUNT', trackList) != '1':
-		print('Polywav detected... aborting work on ' + file)
-		return
-	channelIndex = xmlSplit('CHANNEL_INDEX', trackList)
-	print(channelIndex)
-	name = xmlSplit('NAME', trackList)
+	filePathHead, fileName = os.path.split(file)
+	xmlTree = ET.ElementTree(ET.fromstring(fileHeader(file)))
+	#The test for polywav and type... TRACK_COUNT > 1. Polywav. CURRENT_FILENAME != fileName. Split.
+	for elem in xmlTree.iterfind('HISTORY/CURRENT_FILENAME'):
+		currentFileName = elem.text
+	for elem in xmlTree.iterfind('TRACK_LIST/TRACK_COUNT'):
+		trackCount = elem.text
+	if trackCount != '1':
+		print('Polywav detected...')
+		if currentFileName[-5:].lower() == fileName[-5:].lower():
+			print('Appears to be genuine polywav. Aborting...')
+			return
+		else:
+			print('Appears to be a polywav that\'s been split...')
+			interleaveIndex = fileName[-6:][:2].replace('_','')
+	else:
+		interleaveIndex = '1'
+	channelIndex = xmlTree.find('.//TRACK[INTERLEAVE_INDEX="' + interleaveIndex + '"]/CHANNEL_INDEX').text
+	name = xmlTree.find('.//TRACK[INTERLEAVE_INDEX="' + interleaveIndex + '"]/NAME').text.replace(' ','_')
 	print('Track number: ' + channelIndex)
 	print('Name: ' + name)
-	head, tail = os.path.split(file)
-	newFileName = name + '_' + tail[:-6] + '_' + channelIndex + '.WAV'
-	os.rename(os.path.join(head, tail), os.path.join(head, newFileName))
-	return newFileName	
+	newFileName = name + '_' + fileName[:-6] + '_' + channelIndex + '.WAV'
+	os.rename(os.path.join(filePathHead, fileName), os.path.join(filePathHead, newFileName))
+	return newFileName
 
 for x in filesList:
 	if (x[-4:] == '.wav') or (x[-4:] == '.WAV') :
